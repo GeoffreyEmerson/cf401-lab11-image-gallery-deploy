f8506188daa4058ecc77073804a718a59c9082b1
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-09-26 07:02:26 -0700

    added cors

--- app.js
+++ app.js
@@ -20,14 +20,6 @@ module.exports = app
 .use(cors)
 .get('/', (req,res) => res.sendFile(indexHtml))
 
-.use( (req, res, next) => {
-  const url = '*';
-  res.header( 'Access-Control-Allow-Origin', url );
-  res.header( 'Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE' );
-  res.header( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept' );
-  next();
-})
-
 .use('/api/auth', auth)
 .use('/api/images', authCheck, images)
 .use('/api/galleries', authCheck, galleries)
